8c51921b8f400acf326d7b265e236dc4b2287880
andriipasternak31 <andriipasternak31@gmail.com>
2024-03-19 22:18:02 +0200

    taskScheduler update

--- taskScheduler/taskScheduler.java
+++ taskScheduler/taskScheduler.java
@@ -7,23 +7,26 @@ class Solution {
     // -> identical tasks must be separated by at least n intervals due to cooling time.
 
     // ​Return the minimum number of intervals required to complete all tasks.
-
-     public static void leastInterval(char[] tasks) {
+     public int leastInterval(char[] tasks, int n) {
+        //Runtime 14 ms
+        //Beats 57.47% of users with Java
         Map<Character, Integer> m = new HashMap<Character, Integer>();
 
-        for(int i = 0; i< tasks.length; i++){
-
-            if(m.containsKey(tasks[i])){
-    
-                m.put(tasks[i], m.get(tasks[i]) + 1);
-            }
-            else {
-                m.put(tasks[i], 1);
-            }
+        for (char task : tasks) {
+            m.put(task, m.getOrDefault(task, 0) + 1);
         }
-        System.out.println(m);
+
+        int maxFrequency = Collections.max(m.values()); // Тепер цей рядок знаходиться після заповнення мапи
+        
+        int maxCount = Collections.frequency(m.values(), maxFrequency);
+
+        int ans = (maxFrequency - 1) * (n + 1) + maxCount;
+
+        return Math.max(ans, tasks.length);
     }
     public static void main(String[] args) {
-        
+        Solution s = new Solution();
+        char[] arr = {'A', 'A', 'A', 'B', 'B', 'B'};
+        System.out.println(s.leastInterval(arr, 2));
     }
 }
