5193b94da8426373d390a7ef526197a37e21f08e
andriipasternak31 <andriipasternak31@gmail.com>
2024-03-21 14:37:41 +0200

    Remove duplicates solution refactor :-)

--- removeDuplicates/removeDuplicates_p1.java
+++ removeDuplicates/removeDuplicates_p1.java
@@ -19,7 +19,6 @@ class Solution {
         if (nums.length == 0) return 0;
         int insertPos = 1;
 
-        System.out.println();
         for(int i = 1; i < nums.length; i++){
             if(nums[i] != nums[i - 1]){
                 nums[insertPos] = nums[i];
